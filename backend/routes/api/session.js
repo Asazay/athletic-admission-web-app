b1b08204914e31509a8345d87a3b524bd67f13b2
Asazay Luvaiku <asazayluvaiku@gmail.com>
2025-03-29 16:55:45 -0700

    Validate login complete

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -4,8 +4,21 @@ const {Op} = require('sequelize');
 const bcrypt = require('bcryptjs');
 const { setTokenCookie, restoreUser} = require('../../utils/auth');
 const { User } = require('../../db/models');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
+
+const validateLogin = [
+    check('credential')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .withMessage('Please provide a valid email'),
+    check('password')
+      .exists({ checkFalsy: true })
+      .withMessage('Please provide a password.'),
+    handleValidationErrors
+  ];
 
 // Restore session user
 router.get(
@@ -28,6 +41,7 @@ router.get(
 // Log in
 router.post(
     '/',
+    validateLogin,
     async (req, res, next) => {
       const { credential, password } = req.body;
   
