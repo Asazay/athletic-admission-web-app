0f183dc21db51006ac99c5ea69697fd29af9ee61
Asazay Luvaiku <asazayluvaiku@gmail.com>
2025-04-15 17:56:01 -0700

    Made user model changes

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      User.belongsTo(models.School);
+      User.belongsTo(models.School, {foreignKey: 'schoolId'});
     }
   }
   User.init({
